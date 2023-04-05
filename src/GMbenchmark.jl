ce27c4a2de3f26bbf41e44d7f02fd3859e74ed99
Glouton01 <erwan77meunier@outlook.fr>
2023-04-05 19:21:23 +0200

    =benchmark ok

--- src/GMbenchmark.jl
+++ src/GMbenchmark.jl
@@ -8,10 +8,9 @@ const maxTrial = 20
 const maxTime = 20
 
 function benchmarkGM()
-    
     filenames::Vector{String} = readdir(instanceDirectory)
-    for max_ratio_bv_pr in 0:0.1:1
-        for max_ratio_bv_gi in 0:0.1:1
+    for max_ratio_bv_pr in 1:0.25:1
+        for max_ratio_bv_gi in (0.25:0.25:1)
             qualities = Vector{Float64}()
             nbcyclestotalVect = Vector{Int64}()
             nbcyclesMaxVect = Vector{Int64}()
